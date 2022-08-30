e80725afb1f9f85742e1188aed3662f7ac616f7d
Max Koon <max@koon.dev>
2022-08-30 09:01:05 -0400

    feat: add in command result

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 <img height="150px" alt="cane logo" src="https://koon.dev/dev/cane/cane.png"/>
 
-Cane is an extensible terminal program for quick autocompletion.
+Cane is an extensible terminal program for quick autosuggestions.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use std::collections::HashMap;
 use std::io::{stdin, stdout, Error, ErrorKind};
 use std::io::Write;
-use std::env;
-use std::fs;
 use std::io;
-use std::fmt;
 use std::process::{Command, Output};
 use std::str::from_utf8;
 
@@ -17,10 +13,11 @@ const MAX_SUGGESTIONS: usize = 5;
 
 
 fn main() {
-    suggesting_start();
+    let command =  "echo \"{}\" | bc";
+    suggesting_start(command);
 }
 
-fn suggesting_start() {
+fn suggesting_start(command: &str) {
     let mut stdout = stdout().into_raw_mode().unwrap();
     let     stdin = stdin();
 
@@ -31,21 +28,31 @@ fn suggesting_start() {
     stdout.flush().unwrap();
 
     let mut suggester: Suggester = Default::default();
-    suggester.suggestions = vec![
-        "hello there 1".to_string(),
-        "hsdfs 2".to_string(),
-        "my name jeff 3".to_string(),
-        "xdxdxd 4".to_string(),
-        "ok there 5".to_string(),
-    ];
 
     for key in stdin.keys() {
         match key.unwrap() {
             Key::Char('\n') => { },
             Key::Ctrl('c') => { write!(stdout, "^C\r\n").unwrap(); break; }
             Key::Down => suggestion_down(&mut suggester),
+            Key::Up => suggestion_up(&mut suggester),
             Key::Backspace => {},
-            Key::Char(key) => insert_char(&mut suggester, key),
+            Key::Char(key) => {
+                insert_char(&mut suggester, key);
+                suggester.suggestions.clear();
+
+                let input: String = suggester.buffer.iter().collect();
+                let cmd = command.replace("{}", &input);
+                let result = get_command_result(&cmd);
+                match result {
+                    Ok(res) => {
+                        for line in res.lines() {
+                            suggester.suggestions.push(format!("{}", line));
+                        }
+                    },
+                    Err(_err) => {}, // nothing happens on an error, that might want to change
+                } 
+
+            },
             _ => {},
         }
 
@@ -61,21 +68,18 @@ fn suggesting_start() {
 struct Suggester {
     buffer: Vec<char>,
     buffer_cursor: usize,
-    // prompt_display: Vec<char>,
     suggestions: Vec<String>,
     suggestion_cursor: usize,
 }
 
 fn suggestion_down(suggester: &mut Suggester) {
-    if suggester.suggestion_cursor >= suggester.suggestions.len() - 1 { return }
-
+    if suggester.suggestion_cursor > suggester.suggestions.len() - 1 { return }
     suggester.suggestion_cursor += 1;
+}
 
-    // suggester.buffer = suggester.suggestions[suggester.suggestion_cursor].chars().collect();
-    // suggester.buffer_cursor = suggester.buffer.len();
-
-
-    // suggester.prompt_display = if suggester.suggestion_cursor > 0 { suggester.suggestions[suggester.suggestion_cursor].chars().collect() } else { suggester.buffer.clone() };
+fn suggestion_up(suggester: &mut Suggester) {
+    if suggester.suggestion_cursor == 0 { return }
+    suggester.suggestion_cursor -= 1;
 }
 
 fn insert_char(suggester: &mut Suggester, x: char) {
@@ -92,48 +96,26 @@ fn insert_char(suggester: &mut Suggester, x: char) {
 
     suggester.buffer.insert(idx,  x);
     suggester.buffer_cursor += 1;
-
-    // suggester.prompt_display = suggester.buffer.clone();
 }
 
 fn render(suggester: &Suggester, prompt: &str, sink: &mut impl Write) -> io::Result<()> {
     let current: String = suggester.buffer.iter().collect();
-    // let display: String = suggester.prompt_display.iter().collect();
 
     // IDK if this is indexing correctly
     let display = if suggester.suggestion_cursor == 0 { current.clone() } else { suggester.suggestions[suggester.suggestion_cursor - 1].clone() };
 
     write!(sink, "\r{}{}{}\r\n", clear::AfterCursor, prompt, &display).unwrap();
 
-    
-
-    // let mut prelines = String::new();
-    // if suggester.suggestion_cursor == 0 {
-    //     prelines += "> ";
-    // }
-    // prelines += &current;
-    // write!(sink, "{}\r\n", prelines)?;
-    
-
-
-    // let base = vec![current];
-    // let idk: Vec<String> = base.into_iter().chain(suggester.suggestions.into_iter()).collect();
-
+    // merge the two arrays
     let mut suggestions: Vec<&String> = vec![&current];
-
     let base = &suggester.suggestions;
-
     suggestions.extend(base.iter());
-
-
-
 
     for (idx, line) in suggestions.iter().take(MAX_SUGGESTIONS).enumerate() {
         let selected = idx == suggester.suggestion_cursor;
         if selected { write!(sink, "> ")? }
         write!(sink, "{}\r\n", line)?;
     }
-
 
     let right = if suggester.suggestion_cursor == 0 { prompt.len() + suggester.buffer_cursor} else { prompt.len() + display.len() };
 
